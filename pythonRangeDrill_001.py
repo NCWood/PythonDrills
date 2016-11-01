2efaebc958691266982e3590abffd37b48835975
Nicholas Wood <fortwoodpublishing@gmail.com>
2016-10-31 21:25:34 -0700

    Add Python range() drill

---
+++ pythonRangeDrill_001.py
@@ -0,0 +1,26 @@
+# Python Range Drill
+#
+# Author: Nicholas Wood (nicholas.cameron.wood@gmail.com
+#
+#--------------------------------------------------------
+
+# Drill One: range() function with one parameter 
+for i in range(4):
+    print(i)
+
+print("\n")
+
+# Drill Two: range() function with three parameters
+for i in range(3,-1,-1):
+    print(i)
+
+print("\n")
+
+# Drill Three: range() function with three parameters 8642
+for i in range(8,1,-2):
+    print(i)
+
+print("\n")
+
+
+
